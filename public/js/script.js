f6fc0fd1e4ed7e5439bfbbe23e22816577407663
Rob Zyskowski <zyskowski.rob@gmail.com>
2013-09-18 10:56:46 -0700

    Done with basic implementation

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,14 @@
 $(function() {
-  
+  //JQuery goes here.
   
 });
+
+$(document).scroll(function () {
+  var y = $(this).scrollTop();
+  if (y > 100) {
+    $('.header').fadeOut('slow');
+  } else {
+    $('.header').fadeIn('slow');
+  }
+});
+
